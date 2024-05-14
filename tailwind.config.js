236ad0127055b23aef2f5b9b4ebbd585cac9389c
mcMineyC <jedi.master@falconhosting.net>
2024-05-13 21:27:14 -0700

    refactor: Remove unnecessary code for canvas drawing
    add basic left panel

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+// const { require } = require('yargs');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('daisyui'),
+    require('@tailwindcss/typography'),
+  ],
+  daisyui: {
+    themes: [
+      "light",
+      "cupcake",
+      "dark",
+      "retro",
+      "black",
+      "coffee",
+      "night",
+      "sunset",
+      "business",
+      "forest",
+      "cmyk",
+      "luxury",
+      "dracula",
+      "lemonade",
+    ]
+  }
+}
