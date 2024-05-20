4f4cea0d72efe85ab207bbf80278d1e79c8cbb75
mcMineyC <jedi.master@falconhosting.net>
2024-05-20 13:27:09 -0700

    Update frontend to use new backend things

--- src/paneworker.js
+++ src/paneworker.js
@@ -10,9 +10,8 @@ onmessage = (e) => {
 function generateHTML(list) {
     var html = '';
     for (var i = 0; i < list.length; i++) {
-        html += `<div id="pane-entry-${list[i].id}" class="sidebar-pattern flex flex-row w-full">
-            <div id="pane-entry-${list[i].id}-text">${list[i].name}</div>
-            <div class="flex-auto"></div>
+        html += `<div id="pane-entry-${list[i].hash}" class="sidebar-pattern flex flex-row w-full">
+            <div id="pane-entry-${list[i].hash}-text" class="tootltip pane-entry-text flex-1 line-clamp-1" data-tip="${list[i].name}">${list[i].name}</div>
             <div class="inline-block line select-none">
                 <span class="material-symbols-outlined align-middle cursor-pointer">text_select_start</span>
                 <span class="material-symbols-outlined align-middle cursor-pointer">edit</span>
