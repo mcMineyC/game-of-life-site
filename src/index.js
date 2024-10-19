aaf72f4937219c8a097d6a66e70ae2c9fc2ed95a
Mixel-MurP-427 <connor.mullarky@outlook.com>
2024-10-19 14:57:34 -0700

    Started progress on adjustable sidebar.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-// var url = window.location.href.substring(window.location.protocol.length + 2, window.location.href.length).split('/')[0].split(':')[0];
+`// var url = window.location.href.substring(window.location.protocol.length + 2, window.location.href.length).split('/')[0].split(':')[0];
 var port = window.location.href.substring(window.location.protocol.length + 2, window.location.href.length).split('/')[0].split(':')[1];
 var url = "https://eatthecow.mooo.com:5050";
-
-refreshPatterns();
+`
+//refreshPatterns();
 
 function refreshPatterns() {
     axios.get(`${url}/patterns/get`).then((response) => {
@@ -21,3 +21,35 @@ function refreshPatterns() {
         console.error(error.stack);
     });
 }
+
+
+
+//All code above is by McMineyC
+//All code below is by Mixel-MurP-427 and possibly inspired by Tivotal (see credits)
+
+function moveDivider () {
+
+    var Divider = document.querySelector(".divider");
+    var sideBar = document.querySelector(".sidebar");
+    var mainBox = document.querySelector(".mainbox");
+
+    function resize (e) {
+        let size = `${e.x}px`;
+        sideBar.style.width = size;
+        Divider.style.left = size;
+        main.style.width = `calc(100% - ${size})`;
+    };
+
+    Divider.addEventListener("mousedown", () => {
+        document.addEventListener("mousemove", resize, false);
+        console.log('moving');
+        },
+      false
+    );
+
+
+    Divider.addEventListener("mouseup", () => {
+        document.removeEventListener("mousemove", resize, false);
+    });
+
+};
